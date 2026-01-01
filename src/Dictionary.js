337d72ed3008b34f1c5332ba0f89a103e4cbbc38
Dajana Hamza <tvojemail@example.com>
2026-01-01 16:55:57 +0100

    Add Dictionary component with API fetch and display

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import "./Dictionary.css"
+import axios from "axios";
 
 
 function Dictionary(){
@@ -8,8 +9,17 @@ const [text, setText] = useState("") // state drži ono što je u inputu
 // riješi submit forme, ovdje ide API
 function search(event) {
     event.preventDefault() //sprječava refresh
-    alert(`Searching for ${text}`) // tu da se prikaže text
+    alert(`Searching for ${text} definition`) // tu da se prikaže text
+
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${text}`
+    axios.get(apiUrl).then(handleResponse);
 }
+
+function handleResponse(response) {
+    console.log(response.data[0])
+}
+
 // prati vrijednosti koje se upisuju u input polje od strane korisnika i ažurira ih
 function handleChange(event) {
     setText(event.target.value) //trenutni text iz inputa -to je ključ
